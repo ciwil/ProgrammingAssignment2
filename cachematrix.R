b976a5500cd9e870a827812de32f934008ecca09
ciwil <czapskiprzemyslaw@tlen.pl>
2014-06-14 18:17:26 +0200

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 # This second programming assignment will require you to write
 # an R function is able to cache potentially time-consuming 
-# computations. For example, taking the mean of a numeric vector 
+# computations. 
+# For example, taking the mean of a numeric vector 
 # is typically a fast operation. However, for a very long vector, 
 # it may take too long to compute the mean, especially if it has to be 
 # computed repeatedly (e.g. in a loop). If the contents of a vector are not changing, 
